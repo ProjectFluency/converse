35281922f72d4fd097cad3500689cb52d6123d9c
prabhasp <prabhas.pokharel@gmail.com>
2017-04-03 15:08:58 -0700

    Adding three screens to the app.

--- app/index.js
+++ app/index.js
@@ -1,22 +1,63 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Platform } from 'react-native';
+import Tabs from 'react-native-tabs';
 
-
-export default class converse extends Component {
+class Record extends Component {
     render() {
-        var reloadString = (Platform.OS === 'android') ?
-            ("Double tap R on your keyboard to reload, \n" +
-             "Shake or press menu button for dev menu") :
-            ("Press Cmd+R to reload,\n Cmd+D or shake for dev menu");
-        return (
-                <View style={styles.container}>
-                    <Text style={styles.welcome}>Converse App</Text>
-                    <Text style={styles.instructions}>
-                    {reloadString}
-                    </Text>
-                </View>
-               )
+        return(<View style={styles.container}>
+                <Text> Coming soon: a Recording screen </Text>
+               </View>);
     }
+}
+class Messages extends Component {
+    render() {
+        return(<View style={styles.container}>
+                <Text> Coming soon: a Messages screen </Text>
+               </View>);
+    }
+}
+class Practice extends Component {
+    render() {
+        return(<View style={styles.container}>
+                <Text> Coming soon: a Practice screen </Text>
+               </View>);
+    }
+}
+export default class converse extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      page: 'practice',
+    };
+  }
+
+  render() {
+    const { page } = this.state;
+    let view = null;
+    if (page === "practice") {
+        view = <Practice />;
+    } else if (page === "record") {
+        view = <Record />;
+    } else if (page === "messages") {
+        view = <Messages />;
+    }
+    return (
+      <View style={styles.container}>
+        <Tabs
+          selected={page}
+          style={styles.tabbar}
+          selectedStyle={{color:'red'}} onSelect={el=>this.setState({page:el.props.name})}
+        >
+            <Text name="record"> Record  </Text>
+            <Text name="messages"> Messages  </Text>
+            <Text name="practice"> Practice </Text>
+        </Tabs>
+
+        {view}
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -33,7 +74,16 @@ const styles = StyleSheet.create({
     instructions: {
         textAlign: 'center',
         color: '#333333',
-        marginBottom: 5,
     },
-
+    vertlist: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    tabbar: {
+        backgroundColor:'white',
+        height: 64,
+        borderTopColor: 'red',
+        borderTopWidth: 2
+    },
 });
