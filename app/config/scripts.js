f4be8e583cf5fee4af63178906b0e15dd1f8a664
prabhasp <prabhas.pokharel@gmail.com>
2017-04-03 16:17:47 -0700

    Adding sound files and a dialog script file.

---
+++ app/config/scripts.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+    chapters:
+        [{name: "01",
+          title: "Response Practice",
+          dialogues: [{name: "01",
+                       title: "I'm sorry to hear that.",
+                       lines: [{audio: "01_01.wav",
+                                text: "I had a really busy week!"},
+                               {audio: "01_02.wav",
+                                text: "Awww, I'm sorry to hear that!"},
+                               {audio: "01_03.wav",
+                                text: "Yeah, I'm excited for the weekend."}]},
+                      {name: "02",
+                       title: "That's Awesome!",
+                       lines: [{audio: "02_01.wav",
+                                text: "I got a new job!"},
+                               {audio: "02_02.wav",
+                                text: "That's awesome!"},
+                               {audio: "02_03.wav",
+                                text: "Yeah, I'm so excited. I've been looking for a while."}]},
+          ]}]};
+
+
